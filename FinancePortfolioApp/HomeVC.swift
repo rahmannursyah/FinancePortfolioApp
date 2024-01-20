a39495fb0c9eaec83b3b748fc72366bdce4e14a1
Rahmannur Rizki Syahputra <rahmannur@getgo.sg>
2024-01-20 18:08:59 +0700

    feat: show the user's portfolio data

--- FinancePortfolioApp/HomeVC.swift
+++ FinancePortfolioApp/HomeVC.swift
@@ -7,36 +7,41 @@
 
 import UIKit
 import Data
-import SwiftyJSON
+import DGCharts
 import Domain
 
 class HomeVC: UIViewController {
     var presenter: HomeViewToPresenter?
 
-	private lazy var nextBtn: UIButton = {
-		let btn = UIButton()
-		btn.backgroundColor = .red
-		btn.setTitle("Next VC", for: .normal)
-		btn.addTarget(self, action: #selector(didTappedBtn), for: .touchUpInside)
-		btn.translatesAutoresizingMaskIntoConstraints = false
-		return btn
+	private lazy var pieChartView: PieChartView = {
+		let pieView = PieChartView()
+		pieView.translatesAutoresizingMaskIntoConstraints = false
+		return pieView
 	}()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
-		view.backgroundColor = .purple
-		
-		view.addSubview(nextBtn)
-		NSLayoutConstraint.activate([
-			nextBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			nextBtn.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-			nextBtn.widthAnchor.constraint(equalToConstant: 100)
-		])
+		setupView()
+		setupConstraints()
 		
 		presenter?.getTransactionData(Bundle: Bundle(identifier: "com.nur.Data")!, filename: "portfolioData")
 	}
 
+	private func setupView() {
+		view.backgroundColor = .white
+		view.addSubview(pieChartView)
+	}
+	
+	private func setupConstraints() {
+		NSLayoutConstraint.activate([
+			pieChartView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			pieChartView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 2),
+			pieChartView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+			pieChartView.widthAnchor.constraint(equalTo: view.widthAnchor)
+		])
+	}
+	
 	@objc private func didTappedBtn() {
 		self.navigationController?.pushViewController(SecondViewController(), animated: true)
 	}
@@ -47,6 +52,26 @@ extension HomeVC: HomePresenterToView {
 		print(name)
 		print(percentage)
 		print(nominal)
+		
+		// 1. Set ChartDataEntry
+		var dataEntries: [ChartDataEntry] = []
+		for i in 0..<name.count {
+			let dataEntry = PieChartDataEntry(value: percentage[i], label: name[i], data: name[i] as AnyObject)
+			dataEntries.append(dataEntry)
+		}
+		// 2. Set ChartDataSet
+		let pieChartDataSet = PieChartDataSet(entries: dataEntries, label: "")
+		pieChartDataSet.colors = [UIColor.red, UIColor.green, UIColor.yellow, UIColor.blue]
+		pieChartDataSet.valueColors = [UIColor.black]
+		pieChartDataSet.entryLabelColor = UIColor.black
+		// 3. Set ChartData
+		let pieChartData = PieChartData(dataSet: pieChartDataSet)
+		let format = NumberFormatter()
+		format.numberStyle = .none
+		let formatter = DefaultValueFormatter(formatter: format)
+		pieChartData.setValueFormatter(formatter)
+		// 4. Assign it to the chart’s data
+		pieChartView.data = pieChartData
 	}
 	
 	func didFailGetTransactionDetail(error: String) {
